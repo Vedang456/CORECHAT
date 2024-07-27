a050c11b28b8ae6f11efd2516b764de87997ffd0
Vedang <vedanglimaye@gmail.com>
2024-07-27 16:53:55 +0530

    Basic Users functionality added to code

--- client.py
+++ client.py
@@ -2,20 +2,21 @@ import requests
 import json
 from time import strftime ,gmtime
 
-FILE = "messages.txt"
+MESSAGES_FILE = "messages.txt"
+USERS_FILE = "Users.txt"
 
-def Menu():
-    print("\nOperation to be done on the messages:")
-    print("1.Add a message")
-    print("2.Delete a message")
-    print("3.List messages")
-    print("4.Exit")
+def main_menu():
+    print("\nMain Menu:")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
 
-
-
-def current_time():
-    var = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    return(var)
+def message_menu():
+    print("\nMessage Menu:")
+    print("1. Add a message")
+    print("2. List Messages")
+    print("3. List Users")
+    print("4. Logout")
 
 
 def Add_Message(msg):
@@ -25,23 +26,85 @@ def Add_Message(msg):
     print(response.json())
 
 
+def List_Messages():
+    url = "http://localhost:5000/messages"
+    response = requests.get(url)
+    print(json.dumps(response.json() , indent=4))
+
+
+def Register():
+    url = "http://localhost:5000/Register"
+    print("Please provide the Following information")
+    first_name = input("First Name: ").strip()
+    last_name = input("Last Name: ").strip()
+    username = input("Username: ").strip()
+    password = input("Password: ").strip()
+    email = input("Email: ").strip()
+
+    payload = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "password": password,
+        "email": email
+    }
+
+    response = requests.post(url, json=payload)
+    print(response.json())
+    
+def List_Users():
+    url = "http://localhost:5000/Users"
+    response = requests.get(url)
+    print(json.dumps(response.json(), indent=4))
+
+    
+    
+def Login():
+    url = "http://localhost:5000/Login"
+    username = input("Username: ").strip()
+    password = input("Password: ").strip()
+    
+    data = {
+        "username": username,
+        "password": password
+    }
+    
+    response = requests.post(url, json=data)
+    print(response.json())
+
+
+
+
 def main():
+    logged_in = False
     while True:
-        Menu()
-        choice = input("Choose an option (1-4): ")
-        if choice == '1':
-            messages = input("Enter message:")
-            Add_Message(messages)
-        # elif choice == '2':
-        #     msg = input("Entet message to be deleted:")
-        #     #Delete_Message(msg)
-        # elif choice == '3':
-        #     #List_Messages()
-        elif choice == '4':
-            print("Exit")
-            break
+        if not logged_in:
+            main_menu()
+            choice = input("Choose an option (1-3): ")
+            if choice == '1':
+                Register()
+            elif choice == '2':
+                logged_in = Login()
+            elif choice == '3':
+                print("Exit")
+                break
+            else:
+                print("Invalid choice, try again.")
         else:
-            print("Invalid choice,try again.")
+            message_menu()
+            choice = input("Choose an option (1-4): ")
+            if choice == '1':
+                messages = input("Enter message:")
+                Add_Message(messages)
+            elif choice == '2':
+                List_Messages()
+            elif choice == '3':
+                List_Users()
+            elif choice == '4':
+                print("Exit")
+                break
+            else:
+                print("Invalid choice,try again.")
 
 
 
--- server.py
+++ server.py
@@ -4,20 +4,54 @@ from flask import Flask ,request, jsonify
 
 
 
-FILE = "messages.txt"
+MESSAGES_FILE = "messages.txt"
+USERS_FILE = "Users.txt"
 app = Flask("d")
 
+def load_users():
+    if os.path.exists(USERS_FILE):
+        with open(USERS_FILE, "r") as file:
+            return json.load(file)
+    return {}
+
+def save_users(Users):
+    with open(USERS_FILE, "w") as file:
+        json.dump(Users, file)
+
+
 def load_messages():
-    if os.path.exists(FILE):
-        with open(FILE, "r") as file:
+    if os.path.exists(MESSAGES_FILE):
+        with open(MESSAGES_FILE, "r") as file:
             data = json.load(file)
             return data
     return []
 
 def save_messages(messages):
-    with open(FILE, "w") as file:
+    with open(MESSAGES_FILE, "w") as file:
         json.dump(messages,file)
 
+
+
+@app.route("/Register",methods = ['POST'])
+def Register():
+    data = request.json
+    Users  = load_users()
+    Users.append(data)
+    save_users(Users)
+    return jsonify("User Registered Succesfully") , 201
+
+
+@app.route("/Users",methods = ['GET'])
+def List_Users():
+    Users = load_users()
+    return Users , 200
+
+
+
+@app.route("/Login",methods = ['POST'])
+def Login():
+    return jsonify("User logged in") , 201
+    
 
 @app.route("/messages",methods = ['POST'])
 def Add_Message():
@@ -29,12 +63,8 @@ def Add_Message():
     
 
 
+@app.route("/messages",methods = ['GET'])
 def List_Messages():
     messages = load_messages()
-    if messages:
-        print("\nList of messages:")
-        for msg in messages:
-            print(f"{msg}")
-    else:
-        print("No messages.")
+    return messages , 200
 
